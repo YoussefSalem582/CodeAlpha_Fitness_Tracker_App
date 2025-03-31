d86bec1365bb61a1139714abba6fef4d9a626ac1
YoussefSalem582 <youssef.salem.hassan582@gmail.com>
2025-03-31 22:09:36 +0200

    add functionality to create and manage new workouts in the home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,16 +10,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // text controller
+  final newWorkoutController = TextEditingController();
 
   // create a new workout
-  void createNewWorkout(){}
+  void createNewWorkout() {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text("Create new Workout"),
+            content: TextField(controller: newWorkoutController),
+            actions: [
+              // save button
+              MaterialButton(onPressed: save, child: Text("save")),
+              // cancel button
+              MaterialButton(onPressed: cancel, child: Text("cancel")),
+            ],
+          ),
+    );
+  }
+
+  // save workout
+  void save() {
+    // get workout name from text controller
+    String newWorkoutName = newWorkoutController.text;
+    // add workout to workoutdata list
+    Provider.of<WorkoutData>(context, listen: false).addWorkout(newWorkoutName);
+
+    // pop dialog box
+    Navigator.pop(context);
+    clear();
+  }
+
+  // cancel workout
+  void cancel() {
+    // pop dialog box
+    Navigator.pop(context);
+    clear();
+  }
+
+  // clear controllers
+  void clear() {
+    newWorkoutController.clear();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
       builder:
           (context, value, child) => Scaffold(
-            appBar: AppBar(title: const Text('Workout Tracker')),
+            appBar: AppBar(
+              title: const Text('Workout Tracker'),
+              centerTitle: true,
+            ),
             floatingActionButton: FloatingActionButton(
               onPressed: createNewWorkout,
             ),
