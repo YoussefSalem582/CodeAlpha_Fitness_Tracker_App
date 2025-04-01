9c79777eab818067c402e5935c139871b959c39b
YoussefSalem582 <youssef.salem.hassan582@gmail.com>
2025-04-02 01:12:19 +0200

    add ExerciseTile component and integrate it into WorkoutPage

--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_tracker_app/components/exercise_tile.dart';
 import 'package:fitness_tracker_app/data/workout_data.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,46 +13,87 @@ class WorkoutPage extends StatefulWidget {
 }
 
 class _WorkoutPageState extends State<WorkoutPage> {
+  // checkbox was tapped
+  void onCheckBoxChanged(String workoutName, String exerciseName) {
+    Provider.of<WorkoutData>(
+      context,
+      listen: false,
+    ).checkOffExercise(workoutName, exerciseName);
+  }
+
+  // text controller
+  final exerciseNameController = TextEditingController();
+  final weightController = TextEditingController();
+  final repsController = TextEditingController();
+  final setsController = TextEditingController();
+
+  // create a new exercise
+  void createNewExercise() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+          title: Text("Add a new exercise"),
+        content: Column(
+          // exercise name
+
+          // weight
+
+          // reps
+
+          // sets
+
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
       builder:
           (context, value, child) => Scaffold(
             appBar: AppBar(title: Text(widget.workoutName)),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => createNewExercise,
+              child: Icon(Icons.add),
+            ),
             body: ListView.builder(
               itemCount: value.numberOfExercises(widget.workoutName),
               itemBuilder:
-                  (context, index) => ListTile(
-                    title: Text(
-                      value
-                          .getReleventWorkout(widget.workoutName)
-                          .exercises[index]
-                          .name,
-                    ),
-                    subtitle: Row(
-                      children: [
-                        // weight
-                        Chip(
-                          label: Text(
-                            "${value.getReleventWorkout(widget.workoutName).exercises[index].weight} kg",
-                          ),
+                  (context, index) => ExerciseTile(
+                    exerciseName:
+                        value
+                            .getReleventWorkout(widget.workoutName)
+                            .exercises[index]
+                            .name,
+                    weight:
+                        value
+                            .getReleventWorkout(widget.workoutName)
+                            .exercises[index]
+                            .weight,
+                    reps:
+                        value
+                            .getReleventWorkout(widget.workoutName)
+                            .exercises[index]
+                            .reps,
+                    sets:
+                        value
+                            .getReleventWorkout(widget.workoutName)
+                            .exercises[index]
+                            .sets,
+                    isCompleted:
+                        value
+                            .getReleventWorkout(widget.workoutName)
+                            .exercises[index]
+                            .isCompleted,
+                    onCheckBoxChanged:
+                        (val) => onCheckBoxChanged(
+                          widget.workoutName,
+                          value
+                              .getReleventWorkout(widget.workoutName)
+                              .exercises[index]
+                              .name,
                         ),
-
-                        // reps
-                        Chip(
-                          label: Text(
-                            "${value.getReleventWorkout(widget.workoutName).exercises[index].reps} reps",
-                          ),
-                        ),
-
-                        // sets
-                        Chip(
-                          label: Text(
-                            "${value.getReleventWorkout(widget.workoutName).exercises[index].sets} sets",
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
             ),
           ),
